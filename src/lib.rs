2ba75173a9dcec8213acba0b77391fec14fb2330
mitchmindtree <mitchell.nordine@gmail.com>
2019-09-24 18:51:30 +1000

    Make library build process more flexible by adding staging
    This adds multiple stages to the library building process. Rather than
    going from:
    
    `Watch::next` -> `Library`
    
    The process now looks like:
    
    `Watch::next` -> `Package` -> `Build` -> `Library`
    
    By adding these intermediary steps, we can allow for the user to provide
    feedback to the user by emitting separate events between when a build
    triggering file system event is detected, when the build process begins,
    when the build process ends and when the library is loaded.
    
    Also, by default, loading a library now first copies the library to the
    system's `std::env::temp_dir` directory before loading it from there.
    This allows the user to continue to use the loaded library while using a
    `hotlib::Watch` to simultaneously build the next library. A
    `hotlib::Build::load_in_place` function has also been added to allow for
    the original behaviour.

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,53 @@
 //! For watching and loading a Rust library in Rust.
 //!
-//! See the [watch](./fn.watch.html) docs.
+//! You are likely looking for the [watch function docs](./fn.watch.html).
 
 use derive_more::From;
 use failure::Fail;
 use notify::Watcher as NotifyWatcher;
 use std::path::{Path, PathBuf};
+use std::time::SystemTime;
+
+#[doc(inline)]
+pub use libloading::{self, Library, Symbol};
 
 /// Watches and re-builds the library upon changes to its source code.
 pub struct Watch {
+    package_info: PackageInfo,
+    watcher: notify::RecommendedWatcher,
+    event_rx: crossbeam_channel::Receiver<notify::RawEvent>,
+}
+
+struct PackageInfo {
     cargo_config: cargo::Config,
     cargo_package: cargo::core::Package,
     cargo_toml_path: PathBuf,
     src_path: PathBuf,
-    watcher: notify::RecommendedWatcher,
-    event_rx: crossbeam_channel::Receiver<notify::RawEvent>,
+}
+
+/// The information required to build the package's dylib target.
+pub struct Package<'a> {
+    info: &'a PackageInfo,
+}
+
+/// The result of building a package's dynamic library.
+///
+/// This can be used to load the dynamic library either in place or via a temporary file as to allow
+/// for re-building the package while using the library.
+pub struct Build<'a> {
+    timestamp: SystemTime,
+    target: &'a cargo::core::manifest::Target,
+    compilation: cargo::core::compiler::Compilation<'a>,
+}
+
+/// A wrapper around a `libloading::Library` that cleans up the library on `Drop`.
+pub struct TempLibrary {
+    build_timestamp: SystemTime,
+    path: PathBuf,
+    // This is always `Some`. An `Option` is only used so that the library may be `Drop`ped during
+    // the `TempLibrary`'s `drop` implementation before the temporary library file at `path` is
+    // removed.
+    lib: Option<libloading::Library>,
 }
 
 /// Errors that might occur within the `watch` function.
@@ -36,18 +69,11 @@ pub enum WatchError {
     },
 }
 
-/// Errors that might occur while waiting for the next loaded library instance.
+/// Errors that might occur while building a library instance.
 #[derive(Debug, Fail, From)]
-pub enum NextError {
+pub enum BuildError {
     #[fail(display = "no dylib targets were found within the given cargo package")]
     NoDylibTarget,
-    #[fail(display = "the channel used to receive file system events was closed")]
-    ChannelClosed,
-    #[fail(display = "a notify event signalled an error: {}", err)]
-    Notify {
-        #[fail(cause)]
-        err: notify::Error,
-    },
     #[fail(display = "an error occurred within cargo: {}", err)]
     Cargo {
         #[fail(cause)]
@@ -63,6 +89,33 @@ pub enum NextError {
         #[fail(cause)]
         err: std::io::Error,
     }
+}
+
+/// Errors that might occur while waiting for the next library instance.
+#[derive(Debug, Fail, From)]
+pub enum NextError {
+    #[fail(display = "the channel used to receive file system events was closed")]
+    ChannelClosed,
+    #[fail(display = "a notify event signalled an error: {}", err)]
+    Notify {
+        #[fail(cause)]
+        err: notify::Error,
+    },
+}
+
+/// Errors that might occur while loading a built library.
+#[derive(Debug, Fail)]
+pub enum LoadError {
+    #[fail(display = "an IO error occurred: {}", err)]
+    Io {
+        #[fail(cause)]
+        err: std::io::Error,
+    },
+    #[fail(display = "failed to load library with libloading: {}", err)]
+    Library {
+        #[fail(cause)]
+        err: std::io::Error,
+    },
 }
 
 /// Watch the library at the given `Path`.
@@ -96,14 +149,194 @@ pub fn watch(path: &Path) -> Result<Watch, WatchError> {
     let cargo_toml_path = path.to_path_buf();
     let src_path = src_dir_path.to_path_buf();
 
-    Ok(Watch {
+    // Collect the package info.
+    let package_info = PackageInfo {
         cargo_config,
         cargo_package,
         cargo_toml_path,
         src_path,
+    };
+
+    Ok(Watch {
+        package_info,
         watcher,
         event_rx,
     })
+}
+
+impl Watch {
+    /// The path to the package's `Cargo.toml`.
+    pub fn cargo_toml_path(&self) -> &Path {
+        &self.package_info.cargo_toml_path
+    }
+
+    /// The path to the source directory being watched.
+    pub fn src_path(&self) -> &Path {
+        &self.package_info.src_path
+    }
+
+    /// Wait for the library to be re-built after some change.
+    pub fn next(&self) -> Result<Package, NextError> {
+        loop {
+            let event = match self.event_rx.recv() {
+                Err(_) => return Err(NextError::ChannelClosed),
+                Ok(event) => event,
+            };
+            if check_raw_event(event)? {
+                return Ok(self.package());
+            }
+        }
+    }
+
+    /// The same as `next`, but returns early if there are no pending events.
+    pub fn try_next(&self) -> Result<Option<Package>, NextError> {
+        for event in self.event_rx.try_iter() {
+            if check_raw_event(event)? {
+                return Ok(Some(self.package()));
+            }
+        }
+        Ok(None)
+    }
+
+    /// Manually retrieve the library's package immediately without checking for file events.
+    ///
+    /// This is useful for triggering an initial build during model initialisation.
+    pub fn package(&self) -> Package {
+        let info = &self.package_info;
+        Package { info }
+    }
+}
+
+impl<'a> Package<'a> {
+    /// The path to the package's `Cargo.toml`.
+    pub fn cargo_toml_path(&self) -> &Path {
+        &self.info.cargo_toml_path
+    }
+
+    /// The path to the source directory being watched.
+    pub fn src_path(&self) -> &Path {
+        &self.info.src_path
+    }
+
+    /// Builds the package's dynamic library target.
+    pub fn build(&self) -> Result<Build<'a>, BuildError> {
+        let PackageInfo {
+            ref cargo_package,
+            ref cargo_config,
+            ..
+        } = self.info;
+
+        // Check there's a dylib target.
+        let target = package_dylib_target(cargo_package).ok_or(BuildError::NoDylibTarget)?;
+
+        // Compile the package.
+        let options = compile_options(cargo_config).map_err(|err| BuildError::Cargo { err })?;
+        let pkg_manifest_path = cargo_package.manifest_path();
+        let pkg_workspace = cargo::core::Workspace::new(&pkg_manifest_path, cargo_config)
+            .map_err(|err| BuildError::Cargo { err })?;
+        let compilation = cargo::ops::compile(&pkg_workspace, &options)
+            .map_err(|err| BuildError::CompilationFailed { err })?;
+        let timestamp = SystemTime::now();
+
+        Ok(Build { timestamp, target, compilation })
+    }
+}
+
+impl<'a> Build<'a> {
+    /// The moment at which the build was completed.
+    pub fn timestamp(&self) -> SystemTime {
+        self.timestamp
+    }
+
+    /// The path to the generated dylib target.
+    pub fn dylib_path(&self) -> PathBuf {
+        let file_stem = self.file_stem();
+        self.compilation.root_output.join(file_stem).with_extension(dylib_ext())
+    }
+
+    /// The path to the temporary dynamic library clone that will be created upon `load`.
+    pub fn tmp_dylib_path(&self) -> PathBuf {
+        tmp_dir().join(self.tmp_file_stem()).with_extension(dylib_ext())
+    }
+
+    /// Copy the library to the platform's temporary directory and load it from there.
+    ///
+    /// Note that the copied dynamic library will be removed on `Drop`.
+    pub fn load(self) -> Result<TempLibrary, LoadError> {
+        let dylib_path = self.dylib_path();
+        let tmp_path = self.tmp_dylib_path();
+
+        // If the library already exists, load it.
+        loop {
+            if tmp_path.exists() {
+                let lib = libloading::Library::new(&tmp_path)
+                    .map(Some)
+                    .map_err(|err| LoadError::Library { err })?;
+                let path = tmp_path;
+                let build_timestamp = self.timestamp;
+                let tmp = TempLibrary { build_timestamp, path, lib };
+                return Ok(tmp);
+            }
+
+            // Copy the dylib to the tmp location.
+            let tmp_dir = tmp_path.parent().expect("temp dylib path has no parent");
+            std::fs::create_dir_all(tmp_dir).map_err(|err| LoadError::Io { err })?;
+            std::fs::copy(&dylib_path, &tmp_path).map_err(|err| LoadError::Io { err })?;
+        }
+    }
+
+    /// Load the library from it's existing location.
+    ///
+    /// Note that if you do this, you will have to ensure the returned `Library` is dropped before
+    /// attempting to re-build the library.
+    pub fn load_in_place(self) -> libloading::Result<libloading::Library> {
+        let dylib_path = self.dylib_path();
+        libloading::Library::new(dylib_path)
+    }
+
+    // The file stem of the built dynamic library.
+    fn file_stem(&self) -> String {
+        format!("lib{}", self.target.name())
+    }
+
+    // Produce the file stem for the temporary dynamic library clone that will be created upon
+    // `load`.
+    fn tmp_file_stem(&self) -> String {
+        format!("{}-{}", self.file_stem(), humantime::format_rfc3339(self.timestamp))
+    }
+}
+
+impl TempLibrary {
+    /// The inner `libloading::Library`.
+    ///
+    /// This may also be accessed via the `Deref` implementation.
+    pub fn lib(&self) -> &libloading::Library {
+        self.lib.as_ref().expect("lib should always be `Some` until `Drop`")
+    }
+
+    /// The time at which the original library was built.
+    pub fn build_timestamp(&self) -> SystemTime {
+        self.build_timestamp
+    }
+
+    /// The path at which the loaded temporary library is located.
+    pub fn path(&self) -> &Path {
+        &self.path
+    }
+}
+
+impl std::ops::Deref for TempLibrary {
+    type Target = libloading::Library;
+    fn deref(&self) -> &Self::Target {
+        self.lib()
+    }
+}
+
+impl Drop for TempLibrary {
+    fn drop(&mut self) {
+        std::mem::drop(self.lib.take());
+        std::fs::remove_file(&self.path).ok();
+    }
 }
 
 // The first dylib target within the given package.
@@ -111,46 +344,9 @@ fn package_dylib_target(pkg: &cargo::core::Package) -> Option<&cargo::core::Targ
     pkg.targets().iter().find(|t| t.is_dylib())
 }
 
-impl Watch {
-    /// The path to the package's `Cargo.toml`.
-    pub fn cargo_toml_path(&self) -> &Path {
-        &self.cargo_toml_path
-    }
-
-    /// The path to the source directory being watched.
-    pub fn src_path(&self) -> &Path {
-        &self.src_path
-    }
-
-    /// Wait for the library to be re-built after some change.
-    pub fn next(&self) -> Result<libloading::Library, NextError> {
-        loop {
-            let event = match self.event_rx.recv() {
-                Err(_) => return Err(NextError::ChannelClosed),
-                Ok(event) => event,
-            };
-            if check_raw_event(event)? {
-                return build_and_load(self);
-            }
-        }
-    }
-
-    /// The same as `next`, but returns early if there are no pending events.
-    pub fn try_next(&self) -> Result<Option<libloading::Library>, NextError> {
-        for event in self.event_rx.try_iter() {
-            if check_raw_event(event)? {
-                return build_and_load(self).map(Some);
-            }
-        }
-        Ok(None)
-    }
-
-    /// Manually invoke a build of the watched library.
-    ///
-    /// This is useful for retrieving an initial build during model initialisation.
-    pub fn build(&self) -> Result<libloading::Library, NextError> {
-        build_and_load(self)
-    }
+// The temporary directory used by hotlib.
+fn tmp_dir() -> PathBuf {
+    std::env::temp_dir().join("hotlib")
 }
 
 // Whether or not the given event should trigger a rebuild.
@@ -164,34 +360,6 @@ fn check_raw_event(event: notify::RawEvent) -> Result<bool, NextError> {
     Ok(event
         .op?
         .intersects(Op::CREATE | Op::REMOVE | Op::WRITE | Op::CLOSE_WRITE | Op::RENAME | Op::METADATA))
-}
-
-// Attempt to build the watched library.
-fn build_and_load(watch: &Watch) -> Result<libloading::Library, NextError> {
-    let Watch {
-        ref cargo_config,
-        ref cargo_package,
-        ..
-    } = *watch;
-
-    // Check there's a dylib target.
-    let dylib_target = package_dylib_target(&cargo_package).ok_or(NextError::NoDylibTarget)?;
-
-    // Compile the package.
-    let options = compile_options(cargo_config).map_err(|err| NextError::Cargo { err })?;
-    let pkg_manifest_path = cargo_package.manifest_path();
-    let pkg_workspace = cargo::core::Workspace::new(&pkg_manifest_path, &cargo_config)
-        .map_err(|err| NextError::Cargo { err })?;
-    let compilation = cargo::ops::compile(&pkg_workspace, &options)
-        .map_err(|err| NextError::CompilationFailed { err })?;
-
-    // Locate the generated binary.
-    let file_stem = format!("lib{}", dylib_target.name());
-    let dylib_path = compilation.root_output.join(file_stem).with_extension(dylib_ext());
-
-    // Load the library and return it.
-    let lib = libloading::Library::new(&dylib_path)?;
-    Ok(lib)
 }
 
 // Get the dylib extension for this platform.
