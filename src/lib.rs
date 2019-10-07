ce08e98a8bf48298f9b1e51a992e95c4130fd40e
mitchmindtree <mitchell.nordine@gmail.com>
2019-10-07 12:37:32 +0200

    Remove `cargo` dep in favour of invoking system `cargo` installation
    Closes #7.

--- src/lib.rs
+++ src/lib.rs
@@ -20,10 +20,10 @@ pub struct Watch {
 }
 
 struct PackageInfo {
-    cargo_config: cargo::Config,
-    cargo_package: cargo::core::Package,
-    cargo_toml_path: PathBuf,
+    manifest_path: PathBuf,
     src_path: PathBuf,
+    lib_name: String,
+    target_dir_path: PathBuf,
 }
 
 /// The information required to build the package's dylib target.
@@ -36,9 +36,10 @@ pub struct Package<'a> {
 /// This can be used to load the dynamic library either in place or via a temporary file as to allow
 /// for re-building the package while using the library.
 pub struct Build<'a> {
+    lib_name: &'a str,
+    target_dir_path: &'a Path,
     timestamp: SystemTime,
-    target: &'a cargo::core::manifest::Target,
-    compilation: cargo::core::compiler::Compilation<'a>,
+    output: std::process::Output,
 }
 
 /// A wrapper around a `libloading::Library` that cleans up the library on `Drop`.
@@ -56,10 +57,20 @@ pub struct TempLibrary {
 pub enum WatchError {
     #[fail(display = "invalid path: expected path to end with `Cargo.toml`")]
     InvalidPath,
-    #[fail(display = "a Cargo error occurred: {}", err)]
-    Cargo {
+    #[fail(display = "an IO error occurred while attempting to invoke `cargo metadata`: {}", err)]
+    Io {
         #[fail(cause)]
-        err: failure::Error,
+        err: std::io::Error,
+    },
+    #[fail(display = "{}", err)]
+    ExitStatusUnsuccessful {
+        #[fail(cause)]
+        err: ExitStatusUnsuccessfulError,
+    },
+    #[fail(display = "an error occurred when attempting to read cargo stdout as json: {}", err)]
+    Json {
+        #[fail(cause)]
+        err: serde_json::Error,
     },
     #[fail(display = "no dylib targets were found within the given cargo package")]
     NoDylibTarget,
@@ -73,23 +84,24 @@ pub enum WatchError {
 /// Errors that might occur while building a library instance.
 #[derive(Debug, Fail, From)]
 pub enum BuildError {
-    #[fail(display = "no dylib targets were found within the given cargo package")]
-    NoDylibTarget,
-    #[fail(display = "an error occurred within cargo: {}", err)]
-    Cargo {
-        #[fail(cause)]
-        err: failure::Error,
-    },
-    #[fail(display = "compilation failed: {}", err)]
-    CompilationFailed {
-        #[fail(cause)]
-        err: failure::Error,
-    },
-    #[fail(display = "failed to load dynamic library: {}", err)]
-    LoadingLibraryFailed {
+    #[fail(display = "an IO error occurred while attempting to invoke cargo: {}", err)]
+    Io {
         #[fail(cause)]
         err: std::io::Error,
-    }
+    },
+    #[fail(display = "{}", err)]
+    ExitStatusUnsuccessful {
+        #[fail(cause)]
+        err: ExitStatusUnsuccessfulError,
+    },
+}
+
+/// A process' output indicates unsuccessful completion.
+#[derive(Debug, Fail)]
+#[fail(display = "cargo process exited unsuccessfully with status code: {:?}: {}", code, stderr)]
+pub struct ExitStatusUnsuccessfulError {
+    pub code: Option<i32>,
+    pub stderr: String,
 }
 
 /// Errors that might occur while waiting for the next library instance.
@@ -119,6 +131,19 @@ pub enum LoadError {
     },
 }
 
+impl ExitStatusUnsuccessfulError {
+    /// Produces the error if output indicates failure.
+    pub fn from_output(output: &std::process::Output) -> Option<Self> {
+        // Check for process failure.
+        if !output.status.success() {
+            let code = output.status.code();
+            let stderr = String::from_utf8_lossy(&output.stderr).to_string();
+            return Some(ExitStatusUnsuccessfulError { code, stderr });
+        }
+        None
+    }
+}
+
 /// Watch the library at the given `Path`.
 ///
 /// The given `Path` should point to the `Cargo.toml` of the package used to build the library.
@@ -133,12 +158,57 @@ pub fn watch(path: &Path) -> Result<Watch, WatchError> {
         return Err(WatchError::InvalidPath);
     }
 
-    // Find the package's dylib target src directory.
-    let cargo_config = cargo::Config::default()?;
-    let source_id = cargo::core::SourceId::for_path(path)?;
-    let (cargo_package, _) = cargo::ops::read_package(path, source_id, &cargo_config)?;
-    let target = package_dylib_target(&cargo_package).ok_or(WatchError::NoDylibTarget)?;
-    let src_root_path = target.src_path().path().ok_or(WatchError::NoDylibTarget)?;
+    // Run the `cargo metadata` command to retrieve JSON containing lib target info.
+    let manifest_path_str = format!("{}", path.display());
+    let output = std::process::Command::new("cargo")
+        .arg("metadata")
+        .arg("--manifest-path")
+        .arg(&manifest_path_str)
+        .arg("--format-version")
+        .arg("1")
+        .output()?;
+
+    // Check the exit status.
+    if let Some(err) = ExitStatusUnsuccessfulError::from_output(&output) {
+        return Err(WatchError::from(err));
+    }
+
+    // Read the stdout as JSON.
+    let json: serde_json::Value = serde_json::from_slice(&output.stdout)?;
+
+    // A function to read paths and name out of JSON.
+    fn read_json(json: &serde_json::Value) -> Option<(PathBuf, PathBuf, String)> {
+        let obj = json.as_object()?;
+
+        // Retrieve the target directory.
+        let target_dir_str = obj.get("target_directory")?.as_str()?;
+        let target_dir_path = Path::new(target_dir_str).to_path_buf();
+
+        // Retrieve the first package as an object.
+        let pkgs = obj.get("packages")?.as_array()?;
+        let pkg = pkgs.get(0)?.as_object()?;
+
+        // Search the targets for one containing a dylib output.
+        let targets = pkg.get("targets")?.as_array()?;
+        let target = targets.iter().find_map(|target| {
+            let kind = target.get("kind")?.as_array()?;
+            if kind.iter().find(|k| k.as_str() == Some("dylib")).is_some() {
+                return Some(target);
+            } else {
+                None
+            }
+        })?;
+
+        // Target name and src path.
+        let lib_name = target.get("name")?.as_str()?.to_string();
+        let src_root_str = target.get("src_path")?.as_str()?;
+        let src_root_path = Path::new(src_root_str).to_path_buf();
+
+        Some((target_dir_path, src_root_path, lib_name))
+    }
+
+    let (target_dir_path, src_root_path, lib_name) = read_json(&json)
+        .ok_or(WatchError::NoDylibTarget)?;
     let src_dir_path = src_root_path.parent().expect("src root has no parent directory");
 
     // Begin watching the src path.
@@ -147,15 +217,15 @@ pub fn watch(path: &Path) -> Result<Watch, WatchError> {
     watcher.watch(src_dir_path, notify::RecursiveMode::Recursive)?;
 
     // Collect the paths.
-    let cargo_toml_path = path.to_path_buf();
+    let manifest_path = path.to_path_buf();
     let src_path = src_dir_path.to_path_buf();
 
     // Collect the package info.
     let package_info = PackageInfo {
-        cargo_config,
-        cargo_package,
-        cargo_toml_path,
+        manifest_path,
         src_path,
+        lib_name,
+        target_dir_path,
     };
 
     Ok(Watch {
@@ -167,8 +237,8 @@ pub fn watch(path: &Path) -> Result<Watch, WatchError> {
 
 impl Watch {
     /// The path to the package's `Cargo.toml`.
-    pub fn cargo_toml_path(&self) -> &Path {
-        &self.package_info.cargo_toml_path
+    pub fn manifest_path(&self) -> &Path {
+        &self.package_info.manifest_path
     }
 
     /// The path to the source directory being watched.
@@ -210,8 +280,8 @@ impl Watch {
 
 impl<'a> Package<'a> {
     /// The path to the package's `Cargo.toml`.
-    pub fn cargo_toml_path(&self) -> &Path {
-        &self.info.cargo_toml_path
+    pub fn manifest_path(&self) -> &Path {
+        &self.info.manifest_path
     }
 
     /// The path to the source directory being watched.
@@ -222,28 +292,40 @@ impl<'a> Package<'a> {
     /// Builds the package's dynamic library target.
     pub fn build(&self) -> Result<Build<'a>, BuildError> {
         let PackageInfo {
-            ref cargo_package,
-            ref cargo_config,
+            ref manifest_path,
+            ref lib_name,
+            ref target_dir_path,
             ..
         } = self.info;
 
-        // Check there's a dylib target.
-        let target = package_dylib_target(cargo_package).ok_or(BuildError::NoDylibTarget)?;
+        // Tell cargo to compile the package.
+        let manifest_path_str = format!("{}", manifest_path.display());
+        let output = std::process::Command::new("cargo")
+            .arg("build")
+            .arg("--manifest-path")
+            .arg(&manifest_path_str)
+            .arg("--lib")
+            .arg("--release")
+            .output()?;
 
-        // Compile the package.
-        let options = compile_options(cargo_config).map_err(|err| BuildError::Cargo { err })?;
-        let pkg_manifest_path = cargo_package.manifest_path();
-        let pkg_workspace = cargo::core::Workspace::new(&pkg_manifest_path, cargo_config)
-            .map_err(|err| BuildError::Cargo { err })?;
-        let compilation = cargo::ops::compile(&pkg_workspace, &options)
-            .map_err(|err| BuildError::CompilationFailed { err })?;
+        // Check the exit status.
+        if let Some(err) = ExitStatusUnsuccessfulError::from_output(&output) {
+            return Err(BuildError::from(err));
+        }
+
+        // Time stamp the moment of build completion.
         let timestamp = SystemTime::now();
 
-        Ok(Build { timestamp, target, compilation })
+        Ok(Build { timestamp, output, lib_name, target_dir_path })
     }
 }
 
 impl<'a> Build<'a> {
+    /// The output of the cargo process.
+    pub fn cargo_output(&self) -> &std::process::Output {
+        &self.output
+    }
+
     /// The moment at which the build was completed.
     pub fn timestamp(&self) -> SystemTime {
         self.timestamp
@@ -252,7 +334,7 @@ impl<'a> Build<'a> {
     /// The path to the generated dylib target.
     pub fn dylib_path(&self) -> PathBuf {
         let file_stem = self.file_stem();
-        self.compilation.root_output.join(file_stem).with_extension(dylib_ext())
+        self.target_dir_path.join("release").join(file_stem).with_extension(dylib_ext())
     }
 
     /// The path to the temporary dynamic library clone that will be created upon `load`.
@@ -301,11 +383,11 @@ impl<'a> Build<'a> {
         // A proper solution would likely involve retrieving the file stem from cargo itself.
         #[cfg(target_os = "windows")]
         {
-            format!("{}", self.target.name())
+            format!("{}", self.lib_name)
         }
         #[cfg(not(target_os = "windows"))]
         {
-            format!("lib{}", self.target.name())
+            format!("lib{}", self.lib_name)
         }
     }
 
@@ -348,11 +430,6 @@ impl Drop for TempLibrary {
         std::mem::drop(self.lib.take());
         std::fs::remove_file(&self.path).ok();
     }
-}
-
-// The first dylib target within the given package.
-fn package_dylib_target(pkg: &cargo::core::Package) -> Option<&cargo::core::Target> {
-    pkg.targets().iter().find(|t| t.is_dylib())
 }
 
 // The temporary directory used by hotlib.
@@ -398,12 +475,4 @@ fn dylib_ext() -> &'static str {
     {
         panic!("unknown dynamic library for this platform")
     }
-}
-
-// The default compile options used for building the dynamic library.
-fn compile_options(conf: &cargo::Config) -> Result<cargo::ops::CompileOptions, failure::Error> {
-    let mode = cargo::core::compiler::CompileMode::Build;
-    let mut opts = cargo::ops::CompileOptions::new(conf, mode)?;
-    opts.build_config.release = true;
-    Ok(opts)
 }
